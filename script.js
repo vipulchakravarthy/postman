a5687f1245fa31cd313102db37122a2a5f7c95aa
vipulchakravarthy <saivipul15@msitprogram.net>
2021-07-12 19:52:42 +0530

    axios request working

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 
+import axios from 'axios';
+
+const form = document.querySelector("[data-form]")
 const queryParamsContainer = document.querySelector('[data-query-params]');
 const requestHeadersContainer = document.querySelector('[data-request-headers]');
 const keyValueTemplate = document.querySelector('[data-key-value-template]')
@@ -22,4 +25,26 @@ function createKeyValuePair() {
         e.target.closest('[data-key-value-pair]').remove();
     })
     return element;
+}
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    axios({
+        url: document.querySelector('[data-url]').value,
+        method: document.querySelector('[data-method]').value,
+        params: keyValuePairsToObject(queryParamsContainer),
+        headers: keyValuePairsToObject(requestHeadersContainer),
+    }).then((resp) => {
+        console.log(resp)
+    })
+})
+
+function keyValuePairsToObject(container) {
+    const pairs = container.querySelectorAll('[data-key-value-pair]')
+    return [...pairs].reduce((data, pair) => {
+        const key = pair.querySelector('[data-key]').value;
+        const value = pair.querySelector('[data-value]').value;
+        if (key === '') return data;
+        return { ...data, [key]: value }
+    }, {})
 }

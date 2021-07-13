8d4a8b690551a208f1a79afd7c956b69d12da5f0
vipulchakravarthy <saivipul15@msitprogram.net>
2021-07-13 18:09:12 +0530

    completed the post man clone

--- script.js
+++ script.js
@@ -2,11 +2,32 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import axios from 'axios';
+import prettyBytes from 'pretty-bytes';
+
+import setUpEditors from './setupEditor';
 
 const form = document.querySelector("[data-form]")
 const queryParamsContainer = document.querySelector('[data-query-params]');
 const requestHeadersContainer = document.querySelector('[data-request-headers]');
 const keyValueTemplate = document.querySelector('[data-key-value-template]')
+const responseHeadersContainer = document.querySelector('[data-response-headers]');
+
+axios.interceptors.request.use(request => {
+    request.customData = request.customData || {};
+    request.customData.startTime = new Date().getTime();
+    return request;
+})
+
+axios.interceptors.response.use(updateEndTime, e => {
+    return Promise.reject(updateEndTime(e.response))
+})
+
+function updateEndTime(response) {
+    response.customData = response.customData || {};
+    response.customData.time = new Date().getTime() - response.config.customData.startTime
+
+    return response;
+}
 
 document.querySelector('[data-add-query-params-btn]').addEventListener('click', e => {
     queryParamsContainer.append(createKeyValuePair());
@@ -27,17 +48,57 @@ function createKeyValuePair() {
     return element;
 }
 
+const { requestEditor, updateResponseEditor } = setUpEditors();
+
 form.addEventListener('submit', e => {
     e.preventDefault();
+    let data
+    try {
+        data = JSON.parse(requestEditor.state.doc.toString() || null)
+    } catch (e) {
+        alert("JSON data is malformed")
+        return
+    }
     axios({
         url: document.querySelector('[data-url]').value,
         method: document.querySelector('[data-method]').value,
         params: keyValuePairsToObject(queryParamsContainer),
         headers: keyValuePairsToObject(requestHeadersContainer),
-    }).then((resp) => {
-        console.log(resp)
+        data
+    }).then((response) => {
+        document.querySelector('[data-response-section]').classList.remove('d-none')
+        updateResponseDetails(response);
+        updateResponseEditor(response.data);
+        // updateResponseData(response.data);
+        updateResponseHeaders(response.headers);
+    }).catch(error => {
+        error
     })
 })
+
+function updateResponseData(response) {
+    document.querySelector('[data-json-response-body]').innerHTML =
+        `<pre style="word-wrap: break-word; white-space: pre-wrap;">${JSON.stringify(response, null, 2)}</pre>`;
+}
+
+
+function updateResponseDetails(response) {
+    document.querySelector('[data-status]').textContent = response.status;
+    document.querySelector('[data-time]').textContent = response.customData.time;
+    document.querySelector('[data-size]').textContent = prettyBytes(JSON.stringify(response.data).length + JSON.stringify(response.headers).length);
+}
+
+function updateResponseHeaders(headers) {
+    responseHeadersContainer.innerHTML = "";
+    Object.entries(headers).forEach(([key, value]) => {
+        const keyElement = document.createElement('div');
+        keyElement.textContent = key;
+        responseHeadersContainer.append(keyElement);
+        const valueElement = document.createElement('div');
+        valueElement.textContent = value;
+        responseHeadersContainer.append(valueElement);
+    })
+}
 
 function keyValuePairsToObject(container) {
     const pairs = container.querySelectorAll('[data-key-value-pair]')
